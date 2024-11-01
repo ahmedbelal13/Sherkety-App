ccb1ab5fb409e86635261d7d6a41ce931a342774
ahmedbelal13 <ahmed.belal@sherkety.net>
2024-11-02 00:58:32 +0200

    create country model

---
+++ sherkety/apps/sherkety_project/lib/modules/login/data/models/country_model.dart
@@ -0,0 +1,23 @@
+class Country {
+  final String name;
+  final String code;
+  final String flagPath;
+
+  const Country({
+    required this.name,
+    required this.code,
+    required this.flagPath,
+  });
+}
+
+List<Country> countries = [
+  Country(name: 'Brazil', code: '+55', flagPath: 'assets/flags/brazil.png'),
+  Country(name: 'Canada', code: '+1', flagPath: 'assets/flags/canada.png'),
+  Country(name: 'Egypt', code: '+20', flagPath: 'assets/flags/egypt.png'),
+  Country(name: 'France', code: '+33', flagPath: 'assets/flags/france.png'),
+  Country(name: 'Germany', code: '+49', flagPath: 'assets/flags/germany.png'),
+  Country(name: 'Italy', code: '+39', flagPath: 'assets/flags/italy.png'),
+  Country(name: 'Spain', code: '+34', flagPath: 'assets/flags/spain.png'),
+  Country(name: 'United Kingdom', code: '+44', flagPath: 'assets/flags/uk.png'),
+  Country(name: 'United States', code: '+1', flagPath: 'assets/flags/us.png'),
+];
